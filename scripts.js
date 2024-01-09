911d6a5a4c11a0ae3d11f65f7f37add9acf5195d
Sofia Bedinelli <bedinellisofi@gmail.com>
2024-01-09 19:39:13 -0300

    fotos

--- scripts.js
+++ scripts.js
@@ -10,18 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const productos = [
-  { id: 1, nombre: 'Corte de Pelo', precio: 2000 },
-  { id: 2, nombre: 'Afeitado', precio: 1570 },
-  { id: 3, nombre: 'Crema para Barba', precio: 2570 },
-  { id: 4, nombre: 'Navaja de Afeitar', precio: 3050 },
-  { id: 5, nombre: 'Aceite para Barba', precio: 1800 },
-  { id: 6, nombre: 'Set de Barbería Completo', precio: 8000 },
-  { id: 7, nombre: 'Peine de Madera', precio: 1050 },
-  { id: 8, nombre: 'Tijeras de Barbero', precio: 2200 },
-  { id: 9, nombre: 'Gel de Peinado', precio: 1210 },
-  { id: 10, nombre: 'Capa de Barbero', precio: 1500 },
-  { id: 11, nombre: 'Bálsamo para Barba', precio: 2800 },
-  { id: 12, nombre: 'Gel para Cabello', precio: 2800 },
+  { id: 1, nombre: 'Corte de Pelo', precio: 2000, imagen: 'img/32d055a9aa13c212e3587d65a89c17d1609d6372.XL2_.jpg' },
+  { id: 2, nombre: 'Afeitado', precio: 1570, imagen: 'img/descarga (1).jpeg' },
+  { id: 3, nombre: 'Crema para Barba', precio: 2570, imagen: 'img/images.jpeg' },
+  { id: 4, nombre: 'Navaja de Afeitar', precio: 3050, imagen: 'img/navaja-de-corte-dorada-uso-profesional-barberia-peluqueria-d_nq_np_848252-mla31115418304_062019-f1-2fb623c948e61b012f15663921653298-1024-1024.jpg' },
+  { id: 5, nombre: 'Aceite para Barba', precio: 1800, imagen: 'img/Crema-de-afeitado-Captain-Cook-200x286.jpg' },
+  { id: 6, nombre: 'Set de Barbería Completo', precio: 8000, imagen: 'img/Kit-19.jpg' },
+  { id: 7, nombre: 'Peine de Madera', precio: 1050, imagen: 'img/88cd07719609976903094867cc1fef68.jpg' },
+  { id: 8, nombre: 'Tijeras de Barbero', precio: 2200, imagen: 'img/images (1).jpeg' },
+  { id: 9, nombre: 'Gel de Peinado', precio: 1210, imagen: 'img/RWLG8742.jpg' },
+  { id: 10, nombre: 'Capa de Barbero', precio: 1500, imagen: 'img/descarga.jpeg' },
+  { id: 11, nombre: 'Bálsamo para Barba', precio: 2800, imagen: 'img/71bs6EV0gyL.jpg' },
+  { id: 12, nombre: 'Gel para Cabello', precio: 2800, imagen: 'img/descarga (2).jpeg' },
 ];
 
 let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
@@ -135,6 +135,7 @@ function initCatalogo() {
     productoElement.classList.add('producto');
     productoElement.innerHTML = `
           <h3>${producto.nombre}</h3>
+          <img src="${producto.imagen}" alt="${producto.nombre}" class="producto-imagen">
           <p>Precio: $${producto.precio.toFixed(2)}</p>
           <button class="btn btn-agregar" data-id="${producto.id}">Agregar al Carrito</button>
       `;
@@ -144,6 +145,7 @@ function initCatalogo() {
     btnAgregar.addEventListener('click', () => agregarAlCarrito(producto.id));
   });
 }
+
 
 function initCarrito() {
   renderizarCarrito();
